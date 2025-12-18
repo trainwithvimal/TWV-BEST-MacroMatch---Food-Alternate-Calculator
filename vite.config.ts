a23ca3e7073bbbb894a077d9f34f926c66a4e5f4
trainwithvimal <trainwithvimal@gmail.com>
2025-12-18 14:25:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: "/TWV-BEST-MacroMatch---Food-Alternate-Calculator/",
       server: {
         port: 3000,
         host: '0.0.0.0',
